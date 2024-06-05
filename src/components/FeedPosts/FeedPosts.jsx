dfad2af6bc9846ed01debf91b10bf542e01624dc
revenger77025 <143523189+revenger77025@users.noreply.github.com>
2024-06-05 16:10:23 +0530

    Update FeedPosts.jsx
    textupdate

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -38,9 +38,9 @@ const FeedPosts = () => {
       {!isLoading && posts.length === 0 && (
         <>
           <Text fontSize={"md"} color={"red.400"}>
-            Daamn,bro. Looks like you don&apos;t have any friends.
+            you cannnot see a post cause you are not following any user
           </Text>
-          <Text color={"red.400"}>Stop coding and go make some!!</Text>
+          <Text color={"red.400"}>try following some users!!!</Text>
         </>
       )}
     </Container>
